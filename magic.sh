f0e551b57cad1b56c79f8b8440a339ea72f96143
yashwankhede <yashwankhede003@gmail.com>
2025-01-13 14:00:00 +0000

    updates for 2025-01-13

--- magic.sh
+++ magic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start date
-start_date="2024-01-08"
+start_date="2024-01-01"
 current_date=$start_date
 last_date=$start_date
 
@@ -32,8 +32,8 @@ shuffle() {
   done
 }
 
-# Loop for 60 days
-for (( i=1; i<=60; i++ )); do
+# Loop for 365 days
+for (( i=1; i<=365; i++ )); do
     # Determine if it's a commit day or not (60% chance of a commit)
     commit_day=$((RANDOM % 10))
     if [ $commit_day -lt 6 ]; then
